ad20fa161b5da0bd6e12626207f4f52f0407ff09
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-28 14:44:42 +0530

    [Manish Thakur]: Added 2nd program

--- Program.cs
+++ Program.cs
@@ -1,41 +1,47 @@
 namespace Age_Determination_Problem
 {
     public class AgeCategorizer
+{
+    public static string CategorizeAge(int age)
     {
-        public static string CategorizeAge(int age)
+        if (age >= 1 && age <= 14)
+            return "Children";
+        else if (age >= 15 && age <= 24)
+            return "Youth";
+        else if (age >= 25 && age <= 64)
+            return "Adults";
+        else if (age >= 65)
+            return "Seniors";
+        else
+            throw new ArgumentNullException("age", "Age cannot be null.");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter age:");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
         {
-            if (age >= 1 && age <= 14)
-                return "Children";
-            else if (age >= 15 && age <= 24)
-                return "Youth";
-            else if (age >= 25 && age <= 64)
-                return "Adults";
-            else if (age >= 65)
-                return "Seniors";
-            else
-                throw new ArgumentOutOfRangeException("age", "Invalid age entered.");
+            Console.WriteLine("Age cannot be empty.");
+            return;
         }
 
-        public static void Main()
+        if (!int.TryParse(input, out int age))
         {
-            Console.WriteLine("Enter age:");
-            string input = Console.ReadLine();
+            Console.WriteLine("Invalid age entered.");
+            return;
+        }
 
-            if (!int.TryParse(input, out int age))
-            {
-                Console.WriteLine("Invalid age entered.");
-                return;
-            }
-
-            try
-            {
-                string category = CategorizeAge(age);
-                Console.WriteLine("Person's category: " + category);
-            }
-            catch (ArgumentOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+        try
+        {
+            string category = CategorizeAge(age);
+            Console.WriteLine("Person's category: " + category);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
+}
 }
